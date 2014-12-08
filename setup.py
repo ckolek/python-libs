c9bab7f392da34a363cc1547a568f6b83b122562
Chris Kolek <christopher.w@kolek.me>
2014-12-08 12:26:48 -0500

    Add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+PACKAGES = [
+    'cwk',
+]
+
+setup(
+        name='cwk',
+        version='1.0',
+        description='CWK Python Libraries',
+        author='Christopher W. Kolek',
+        author_email='christopher.w@kolek.me',
+        url='christopher-w.kolek.me',
+        packages=PACKAGES,
+        package_dir={'cwk': 'lib'}
+    )
